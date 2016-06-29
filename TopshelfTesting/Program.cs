255b7e5f49f7e0ea7bbc696b0084def8932446bd
divandutoit <malhoek@gmail.com>
2016-06-29 09:13:05 +0200

    Added Solution

---
+++ TopshelfTesting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf;
+
+namespace TopshelfTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(
+            x =>
+            {
+                x.Service<TownCrier>(
+                    s =>
+                    {
+                        s.ConstructUsing(name => new TownCrier());
+                        s.WhenStarted(tc => tc.Start());
+                        s.WhenStopped(tc => tc.Stop());
+                    });
+                x.RunAsLocalSystem();
+                x.SetDescription("TopshelfInstaller");
+                x.SetDisplayName("TopshelfInstaller");
+                x.SetServiceName("TopshelfInstaller");
+            });
+        }
+    }
+
+    public class TownCrier
+    {
+        private readonly Timer _timer;
+
+        public TownCrier()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ TopshelfTesting/ProjectInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopshelfTesting
+{
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceInstaller _serviceInstaller;
+
+        private ServiceProcessInstaller _serviceProcessInstaller;
+
+        public ProjectInstaller()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this._serviceInstaller = new ServiceInstaller();
+            _serviceProcessInstaller = new ServiceProcessInstaller();
+
+            this._serviceProcessInstaller.Account = ServiceAccount.User;
+
+            this._serviceInstaller.DisplayName = "TopshelfInstaller";
+            this._serviceInstaller.ServiceName = "TopshelfInstaller";
+
+            this.Installers.AddRange(new Installer[] { this._serviceProcessInstaller, this._serviceInstaller });
+        }
+    }
+}
